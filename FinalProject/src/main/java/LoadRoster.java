0fac203d8886752735900546034e3b4d1983134c
jlnguyen99 <68876229+jlnguyen99@users.noreply.github.com>
2020-11-17 01:44:27 -0700

    Created functions in LoadRoster.java to get roster from a csv file and load it into a table. Added a student class.

--- FinalProject/src/main/java/LoadRoster.java
+++ FinalProject/src/main/java/LoadRoster.java
@@ -1,5 +1,82 @@
 import javax.swing.*;
+import javax.swing.event.TableModelEvent;
+import javax.swing.event.TableModelListener;
+import javax.swing.filechooser.FileFilter;
+import javax.swing.filechooser.FileNameExtensionFilter;
+import javax.swing.table.TableModel;
 
-public class LoadRoster {
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.BufferedReader;
+import java.util.ArrayList;
+
+public class LoadRoster implements TableModelListener {
+	public ArrayList<Student> studentList = new ArrayList<Student>();
 	
+	public void getRoster() {
+		JFileChooser chooser = new JFileChooser();
+		
+		FileFilter filter = new FileNameExtensionFilter("CSV Files", "csv");
+		
+		chooser.setFileFilter(filter);
+		
+		chooser.showOpenDialog(null);
+		
+		File csvFile = chooser.getSelectedFile();
+		
+		try {
+			FileReader fReader = new FileReader(csvFile);
+			BufferedReader bReader = new BufferedReader(fReader);
+			String line = "";
+			String[] inputs;
+			
+			while ((line = bReader.readLine()) != null) {
+				inputs = line.split(",");
+
+				String id = inputs[0];
+				String firstName = inputs[1];
+				String lastName = inputs[2];
+				String program = inputs[3];
+				String level = inputs[4];
+				String asurite = inputs[5];
+				
+				Student student = new Student(id, firstName, lastName, program, level, asurite);
+				
+				studentList.add(student);
+			}
+			
+			bReader.close();
+		} catch (IOException ioe) {
+			System.out.println("Can't read file");
+		}
+		
+	}
+	
+	public void loadRoster() {
+		String[] columnNames = {"ID", "First Name", "Last Name", "Program", "Level", "ASURITE"};
+		String[][] data = new String[studentList.size()][6];
+		
+		for (int i = 0; i < studentList.size(); i++) {
+			data[i][0] = studentList.get(i).getId();
+			data[i][1] = studentList.get(i).getFirstName();
+			data[i][2] = studentList.get(i).getLastName();
+			data[i][3] = studentList.get(i).getProgram();
+			data[i][4] = studentList.get(i).getLevel();
+			data[i][5] = studentList.get(i).getAsurite();
+		}
+		
+		JScrollPane pane = new JScrollPane();
+		JTable table = new JTable(data, columnNames);
+		
+		table.getModel().addTableModelListener(this);
+		
+		pane.add(table);
+	}
+
+	@Override
+	public void tableChanged(TableModelEvent e) {
+		// TODO Auto-generated method stub
+		TableModel model = (TableModel)e.getSource();
+	}
 }
---
+++ FinalProject/src/main/java/Student.java
@@ -0,0 +1,41 @@
+public class Student {
+	private String id;
+	private String firstName;
+	private String lastName;
+	private String program;
+	private String level;
+	private String asurite;
+	
+	public Student (String id, String fName, String lName, String program, 
+			String level, String asurite) {
+		this.id = id;
+		firstName = fName;
+		lastName = lName;
+		this.program = program;
+		this.asurite = asurite;
+	}
+	
+	public String getId() {
+		return id;
+	}
+	
+	public String getFirstName() {
+		return firstName;
+	}
+	
+	public String getLastName() {
+		return lastName;
+	}
+	
+	public String getProgram() {
+		return program;
+	}
+	
+	public String getLevel() {
+		return level;
+	}
+	
+	public String getAsurite() {
+		return asurite;
+	}
+}
