9ec13755d8b175b841e8a3252be63f74012f8568
Andrew Tran <atran32@asu.edu>
2020-11-29 00:54:33 -0700

    Added imports, laid out functions

--- FinalProject/src/main/java/Save.java
+++ FinalProject/src/main/java/Save.java
@@ -1,5 +1,46 @@
 import javax.swing.*;
+import javax.swing.event.TableModelEvent;
+import javax.swing.event.TableModelListener;
+import javax.swing.filechooser.FileFilter;
+import javax.swing.filechooser.FileNameExtensionFilter;
+import javax.swing.table.TableModel;
 
-public class Save {
+import java.io.*;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.ArrayList;
+
+/* Save should be similar to load attendance, at least for exporting
+ * Import FileReader is swapped with FileWriter
+ * 
+ * Look for the existing JTable and append the data as needed
+ * Make sure to flush + close the stream when finished.
+ * 
+ * File will probably be named as given from the user
+ * Find a way to allow for user to name the saved file
+ * 
+ * 
+*/
+public class Save implements TableModelListener{
+	
+	// Preinitialized parameters
+	private static final char DEFAULT_SEPARATOR = ',';
+	public String[][] streamData;
+	public Save() {}
+	
+	// Function to save the file.
+	// The parameters are place-holders currently, fix later.
+	public void saveFile(JTable table,File fileName) throws IOException{
+		
+	}
+	
+	// Auto-gen'd file to keep TableModel from being angry.
+	@Override
+	public void tableChanged(TableModelEvent arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+	
 	
 }
