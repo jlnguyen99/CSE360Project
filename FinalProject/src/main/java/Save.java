072fba9fe542bd7526f2eff087b730c54e58813c
Andrew Tran <atran32@asu.edu>
2020-11-29 17:45:59 -0700

    Imp file selection, needs writing left

--- FinalProject/src/main/java/Save.java
+++ FinalProject/src/main/java/Save.java
@@ -8,6 +8,7 @@ import javax.swing.table.TableModel;
 import java.io.*;
 import java.io.File;
 import java.io.FileWriter;
+import java.io.BufferedWriter;
 import java.io.IOException;
 import java.util.ArrayList;
 
@@ -20,18 +21,39 @@ import java.util.ArrayList;
  * File will probably be named as given from the user
  * Find a way to allow for user to name the saved file
  * 
- * Yea.
+ * In the case of matching save files, go for an overwrite prompt
+ * 
 */
 public class Save implements TableModelListener{
 	
 	// Preinitialized parameters
 	private static final char DEFAULT_SEPARATOR = ',';
+	public ArrayList<Student> studentList = new ArrayList<Student>();
 	public String[][] streamData;
 	public Save() {}
 	
 	// Function to save the file.
 	// The parameters are place-holders currently, fix later.
-	public void saveFile(JTable table,File fileName) throws IOException{
+	public void saveFile(JTable table) throws IOException{
+		
+		JFileChooser selection = new JFileChooser();
+		
+		FileFilter filter = new FileNameExtensionFilter("CSV Files", "CSV");
+		
+		selection.setFileFilter(filter);
+		selection.showSaveDialog(null);
+		
+		String path = selection.getSelectedFile().getAbsolutePath();
+		String fileName = selection.getSelectedFile().getName();
+		FileWriter csvFile = new FileWriter(fileName);
+		
+		try {
+			FileWriter writer = new FileWriter(fileName);
+			BufferedWriter buffWrite = new BufferedWriter(writer);
+			
+		}catch(IOException e) {
+			System.out.println("File inaccessible, cannot be read.");
+		}
 		
 	}
 	
